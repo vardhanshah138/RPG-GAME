eec3f93d6bc53ac376dd5340a80ab45fa21469a1
vardhan.shah@pirimidtech.com <vardhan.shah@pirimidtech.com>
2024-06-08 14:15:16 +0530

    Item and key

--- src/main/java/org/example/models/Item.java
+++ src/main/java/org/example/models/Item.java
@@ -1,5 +1,19 @@
 package org.example.models;
 
 public abstract class Item {
+
     private String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
---
+++ src/main/java/org/example/models/Key.java
@@ -0,0 +1,7 @@
+package org.example.models;
+
+public class Key extends Item{
+    public Key(String name) {
+        super(name);
+    }
+}
