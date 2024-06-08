929645460177cad55b6a091ba5be5c2f8667f9e5
vardhan.shah@pirimidtech.com <vardhan.shah@pirimidtech.com>
2024-06-08 14:12:28 +0530

    add items and enemies in room

--- src/main/java/org/example/models/Room.java
+++ src/main/java/org/example/models/Room.java
@@ -1,15 +1,18 @@
 package org.example.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Room {
 
     private String name;
     private List<Character> roomMembers;
+    private List <Item> items;
 
-    public Room(String name, List<Character> roomMembers) {
+    public Room(String name) {
         this.name = name;
-        this.roomMembers = roomMembers;
+        this.roomMembers = new ArrayList<>();
+        this.items = new ArrayList<>();
     }
 
     public String getName() {
@@ -26,5 +29,36 @@ public class Room {
 
     public void setRoomMembers(List<Character> roomMembers) {
         this.roomMembers = roomMembers;
+    }
+
+    public void addEnemy(Enemy enemy){
+        roomMembers.add(enemy);
+        System.out.println("Added enemy " + enemy.getName());
+    }
+    public void addItem(Item item){
+        items.add(item);
+        System.out.println("Added item " + item.getName());
+    }
+
+    public void removeItem(Item item){
+        if(items.contains(item)){
+            items.remove(item);
+            System.out.println("Removed item " + item.getName());
+        }else {
+            System.out.println("The item " + item.getName()  + " is not present");
+        }
+    }
+
+    public void printRoomElements(){
+        printCharacter();
+        printItem();
+    }
+
+    public void printCharacter() {
+        roomMembers.forEach((character) -> System.out.println("The character " + character.getName() + " in the room " + name));
+    }
+
+    public void printItem() {
+        items.forEach((item) -> System.out.println("The item " + item.getName() + " in the room " + name));
     }
 }
