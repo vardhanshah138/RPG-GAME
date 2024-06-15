e947039e94a89cc0df4f5eb9ba2758a1692248aa
vardhan.shah@pirimidtech.com <vardhan.shah@pirimidtech.com>
2024-06-15 15:18:49 +0530

    select and enter in room

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import org.example.models.*;
-import org.example.models.Character;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,33 +8,42 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        Scanner sc = new Scanner(System.in);
+
+        //Room initalization
+        List<Room> rooms = new ArrayList<>();
+        Room currRoom = null;
+
+        Room city = new Room("City");
+        Room desert = new Room("Dessert");
+        Room northIsland = new Room("Snow Island");
+        Room village = new Room("Village");
+
+        rooms.addAll(List.of(city, desert, northIsland, village));
+
+        System.out.println("Select room to enter.");
+        currRoom = printAndSelectRoom(rooms);
 
         Enemy zombie = new Enemy("Zombie", 50,3, "I am a brain dead Zombie I will eat you alive and make you zombie");
         Enemy whiteWalker = new Enemy("White walker", 50, 5, "I stayed in Ice and king in the north afraids of me");
         Enemy alien = new Enemy("Alien", 50, 12, "I came from outer space and i have super weapons");
         Enemy akatsuki = new Enemy("Akatsuki", 50,15, "I am a rogue ninja and abaonded the village after killing the whole clan.");
 
-        Room room = new Room("City");
-        room.addEnemy(zombie);
         Player vardhan = new Player("Vardhan",100, 5, "I am Vardhan Shah from Surat great fighter and coder.");
 
-        while(vardhan.isAlive() && zombie.isAlive()){
-            System.out.println("Press 1 to attack.");
-            if(sc.nextInt() == 1){
-                vardhan.attack(zombie);
-            }
-            zombie.attack(vardhan);
-        }
 
-        //Item in the room test.
-        Key key = new Key("Key");
-        Poison poison = new Poison("Sulphur acid",5);
-        Potion potion = new Potion("Medicine", 10);
-        room.addItem(key);
-        room.addItem(poison);
-        room.addItem(potion);
-        room.printRoomElements();
+    }
+
+    public static Room printAndSelectRoom(List<Room> rooms){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Here are the room details please select room in 0 based indexing.");
+        rooms.forEach(room -> System.out.println(room.getName()));
+        int roomIndex = sc.nextInt();
+        while (!(roomIndex >= 0 && roomIndex < rooms.size())) {
+            System.out.println("Please Enter correct room index");
+            roomIndex = sc.nextInt();
+        }
+        Room currRoom = rooms.get(roomIndex);
+        System.out.println("Please enter in room "+ currRoom.getName());
+        return currRoom;
     }
 }
